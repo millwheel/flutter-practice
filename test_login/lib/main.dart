8ad892efe20bc992a610ba440302b8021bd4ae48
season <sionwer4@naver.com>
2024-07-21 22:26:31 +0900

    feat: fill out app bar

--- test_login/lib/main.dart
+++ test_login/lib/main.dart
@@ -8,30 +8,38 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            Text(
-              'Hello Flutter',
-              style: TextStyle(fontSize: 28),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'email'),
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'password'),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('로그인'),
-            )
-          ],
+        appBar: AppBar(
+          title: Text(
+            'Hello Flutter',
+            style: TextStyle(fontSize: 28),
+          ),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Image.network("https://i.ibb.co/CwzHq4z/trans-logo-512.png"),
+              TextField(
+                decoration: InputDecoration(labelText: 'email'),
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(labelText: 'password'),
+              ),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.all(16),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('로그인'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
