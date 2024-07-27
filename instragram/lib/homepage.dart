025619814f3b230bb9aa2d4c496a7fb959e52771
season <sionwer4@naver.com>
2024-07-27 21:44:54 +0900

    feat: Put diverse image source

--- instragram/lib/homepage.dart
+++ instragram/lib/homepage.dart
@@ -7,6 +7,16 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> images = [
+      "https://i.ibb.co/CQxfdHY/cat1.jpg",
+      "https://i.ibb.co/w6wxdrQ/cat2.jpg",
+      "https://i.ibb.co/GnwVqCd/cat3.jpg",
+      "https://i.ibb.co/1GMKYBy/cat4.jpg",
+      "https://i.ibb.co/cTGzTTX/cat5.jpg",
+      "https://i.ibb.co/47Y5Ct5/cat6.jpg",
+      "https://i.ibb.co/ZW38ngD/cat7.gif",
+    ];
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -27,9 +37,10 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.white,
       ),
       body: ListView.builder(
-        itemCount: 10,
+        itemCount: images.length,
         itemBuilder: (context, index) {
-          return Feed();
+          String image = images[index];
+          return Feed(imageUrl: image);
         },
       ),
     );
