7a69433187294e7e381796868f6e84ba1c60f99e
season <sionwer4@naver.com>
2024-07-28 14:49:53 +0900

    feat: Fill out introduction screen content

--- onboarding/lib/main.dart
+++ onboarding/lib/main.dart
@@ -25,23 +25,47 @@ class Onboarding extends StatelessWidget {
     return Scaffold(
       body: IntroductionScreen(
         pages: [
-          // 페이지1
+          // 첫 번째 페이지
           PageViewModel(
-            title: "Title1",
-            bodyWidget: Text("body1"),
+            title: "빠른 개발",
+            body: "Flutter의 hot reload는 쉽고 UI 빌드를 도와줍니다.",
+            image: Padding(
+              padding: EdgeInsets.all(32),
+              child: Image.network('https://i.ibb.co/2ZQW3Sb/flutter.png'),
+            ),
+            decoration: PageDecoration(
+              titleTextStyle: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              bodyTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
           ),
-
-          // 페이지2
+          // 두 번째 페이지
           PageViewModel(
-            title: "Title2",
-            bodyWidget: Text("body2"),
+            title: "표현력 있고 유연한 UI",
+            body: "Flutter에 내장된 아름다운 위젯들로 사용자를 기쁘게 하세요.",
+            image: Image.network('https://i.ibb.co/LRpT3RQ/flutter2.png'),
+            decoration: PageDecoration(
+              titleTextStyle: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              bodyTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
           ),
         ],
         next: Text("Next", style: TextStyle(fontWeight: FontWeight.w600)),
         done: Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
-        onDone: () {
-          // When done button is press
-        },
+        onDone: () {},
       ),
     );
   }
