b41f89c4ede855351fe68a405fedcee627683321
season <sionwer4@naver.com>
2024-07-28 20:08:14 +0900

    feat: Apply provider package to main dart

--- bucket_list/lib/main.dart
+++ bucket_list/lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'bucket_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => BucketService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
