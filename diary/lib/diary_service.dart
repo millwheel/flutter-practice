7d7456503c9175728fb4e19965d42e507e2df8ec
season <sionwer4@naver.com>
2024-08-13 21:14:35 +0900

    feat: fill in diary service logic

--- diary/lib/diary_service.dart
+++ diary/lib/diary_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Diary {
   String text; // 내용
@@ -16,22 +17,37 @@ class DiaryService extends ChangeNotifier {
 
   /// 특정 날짜의 diary 조회
   List<Diary> getByDate(DateTime date) {
-    // TODO : 아래 내용을 지우고 구현해주세요.
-    return diaryList;
+    return diaryList
+        .where((diary) => isSameDay(date, diary.createdAt))
+        .toList();
   }
 
   /// Diary 작성
   void create(String text, DateTime selectedDate) {
-    // TODO
+    DateTime now = DateTime.now();
+    DateTime createdAt = DateTime(
+      selectedDate.year,
+      selectedDate.month,
+      selectedDate.day,
+      now.hour,
+      now.minute,
+      now.second,
+    );
+    Diary diary = Diary(text: text, createdAt: createdAt);
+    diaryList.add(diary);
+    notifyListeners();
   }
 
   /// Diary 수정
   void update(DateTime createdAt, String newContent) {
-    // TODO
+    Diary diary = diaryList.firstWhere((diary) => diary.createdAt == createdAt);
+    diary.text = newContent;
+    notifyListeners();
   }
 
   /// Diary 삭제
   void delete(DateTime createdAt) {
-    // TODO
+    diaryList.removeWhere((diary) => diary.createdAt == createdAt);
+    notifyListeners();
   }
 }
