87d6786806dc11e3062929fee843835ae49da3ce
season <sionwer4@naver.com>
2024-08-13 20:57:12 +0900

    feat: Add calendar in homepage

--- diary/lib/home_page.dart
+++ diary/lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:diary/diary_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,8 +11,24 @@ class HomePage extends StatelessWidget {
     return Consumer<DiaryService>(
       builder: (context, diaryService, child) {
         return Scaffold(
-          body: Center(
-            child: Text("HomePage"),
+          body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TableCalendar(
+                  firstDay: DateTime.utc(2010, 10, 16),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  focusedDay: DateTime.now(),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Text(
+                    "오늘은 기분이 좋다 🎈",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
